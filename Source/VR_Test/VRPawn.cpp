66794612ec2104c6778d6644a998d00964cb12c3
Johann <johann.schumann0@gmail.com>
2023-03-07 19:57:21 +0900

    WIP Rise and fall on VRPawn

--- Source/VR_Test/VRPawn.cpp
+++ Source/VR_Test/VRPawn.cpp
@@ -11,20 +11,6 @@ AVRPawn::AVRPawn()
 
 }
 
-// Called when the game starts or when spawned
-void AVRPawn::BeginPlay()
-{
-	Super::BeginPlay();
-	
-}
-
-// Called every frame
-void AVRPawn::Tick(float DeltaTime)
-{
-	Super::Tick(DeltaTime);
-
-}
-
 // Called to bind functionality to input
 void AVRPawn::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
@@ -32,18 +18,113 @@ void AVRPawn::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 
 }
 
-bool AVRPawn::ShouldStopRelaxed(float DeltaTime)
+// Called when the game starts or when spawned
+void AVRPawn::BeginPlay()
 {
-	m_unrelaxedDuration += DeltaTime;
-	m_unrelaxedFrames.Enqueue({DeltaTime, GetWorld()->GetTimeSeconds()});
+	Super::BeginPlay();
 
-	while (GetWorld()->GetTimeSeconds() - m_unrelaxedFrames.Peek()->timestamp > 5.f)
-	{
-		FFrameStep frameStep;
-		m_unrelaxedFrames.Dequeue(frameStep);
-		m_unrelaxedDuration -= frameStep.dt;
-	}
+	for (int i = 0; i < meditationQueueSize; ++i)
+		m_meditationValues.PushFirst(0);
 
-	return m_unrelaxedDuration > 2.5f;
+	m_interpSpeed = (riseVelocity - fallVelocity) / interpDuration;
+	m_sumSize = meditationQueueSize - 1;
+	m_targetZVelocity = fallVelocity;
 }
 
+// Called every frame
+void AVRPawn::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+	
+	if (m_interpTime <= 1.f)
+		relaxationValue = FMath::Lerp(m_prevAvg, m_currAvg, m_interpTime);
+	
+	m_interpTime += DeltaTime;
+		
+	if (ShouldChangeState())
+	{
+		bRelaxed = !bRelaxed;
+		m_targetZVelocity = bRelaxed ? riseVelocity : fallVelocity;
+
+		//if(bRelaxed && !GetCharacterMovement()->IsFalling())
+		//	LaunchCharacter(FVector(0.f, 0.f, DeltaTime), false, false);
+	}
+	
+	UpdateUpVelocity(DeltaTime);
+}
+
+void AVRPawn::UpdateUpVelocity(float DeltaSeconds)
+{
+	// Ignore if falling but already on ground
+	//if (!bRelaxed && !GetCharacterMovement()->IsFalling())
+	//	return;
+
+	/*auto& z = Movement
+		Velocity.Z;
+	
+	auto& z = GetCharacterMovement()->Velocity.Z;
+	// Interpolate the velocity towards the target velocity
+	if (z != m_targetZVelocity)
+	{
+		if (m_curZVelocity != m_targetZVelocity)
+			m_curZVelocity = FMath::FInterpConstantTo(m_curZVelocity, m_targetZVelocity, DeltaSeconds, m_interpSpeed);
+		z = m_curZVelocity;
+	}*/
+}
+
+void AVRPawn::Landed(const FHitResult& Hit)
+{
+	//if(bRelaxed)
+	//	LaunchCharacter(FVector(0.f, 0.f, m_curZVelocity), false, false);
+}
+
+bool AVRPawn::ShouldChangeState()
+{
+	if (bRelaxed && relaxationValue < 50.f || !bRelaxed && relaxationValue >= 50.f)
+	{
+		int unrelaxedValueCount = 0;
+	
+		for (int i = 0; i < meditationQueueSize; ++i)
+			if (m_meditationValues[i] < 50.f)
+				unrelaxedValueCount++;
+
+		const float unrelaxedRate = static_cast<float>(unrelaxedValueCount) / static_cast<float>(meditationQueueSize);
+
+		return bRelaxed && unrelaxedRate >= oppositeStateThreshold
+			|| !bRelaxed && 1 - unrelaxedRate >= oppositeStateThreshold;
+	}
+	
+	return false;
+}
+
+void AVRPawn::RegisterValue(float value)
+{
+	m_meditationValues.EmplaceFirst(value);
+	m_meditationValues.PopLast();
+	// New value registered, so reset interpTime to 0.
+	m_interpTime = 0.f;
+}
+
+void AVRPawn::AssignValue()
+{
+	m_prevAvg = m_meditationValues.First(); 
+	m_currAvg = m_meditationValues.Last();
+}
+
+void AVRPawn::ComputeAvg()
+{	
+	const float first = m_meditationValues[0];
+	float prev = m_meditationValues[1];
+	float sum = 0.f;
+	
+	for (int i = 2; i < m_meditationValues.Num(); ++i)
+	{
+		sum += prev;
+		prev = m_meditationValues[i];
+	}
+
+	const float last = prev;
+	
+	m_prevAvg = (sum + last) / m_sumSize; 
+	m_currAvg = (sum + first) / m_sumSize;
+}
--- Source/VR_Test/VRPawn.h
+++ Source/VR_Test/VRPawn.h
@@ -3,30 +3,72 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "Containers/Deque.h"
 #include "GameFramework/Pawn.h"
 #include "VRPawn.generated.h"
-
-struct FFrameStep
-{
-	float dt;
-	float timestamp;
-};
 
 UCLASS()
 class VR_TEST_API AVRPawn : public APawn
 {
 	GENERATED_BODY()
 
-	TQueue<FFrameStep> m_unrelaxedFrames;
-	float m_unrelaxedDuration = 0.f;
+	/** Queue of the meditationQueueSize last meditation values stored. End up using a Deque to read the values to compute averages */
+	TDeque<float> m_meditationValues;
+
+	/** Current timer used for the lerping of the relaxation value */
+	float m_interpTime = 0;
+	/** Interpolation speed based on the interpolation duration chosed by the user */
+	float m_interpSpeed;
+	/** Relaxation average of the last "meditationQueueSize" - 1 frames */
+	float m_currAvg = 0;
+	/** Relaxation average of the last "meditationQueueSize" frames, except the most recent one */
+	float m_prevAvg = 0;
+	/** Up velocity the Character is currently aiming for */
+	double m_targetZVelocity;
+	/** Current up velocity of the Character */
+	double m_curZVelocity;
+	/** Number of values m_prevAvg and m_currAvg bases their average on. = meditationQueueSize - 1 */
+	int m_sumSize;
 
 public:
 	// Sets default values for this pawn's properties
 	AVRPawn();
 
+	/** Rise velocity when relaxed */
+	UPROPERTY(EditAnywhere, meta = (ClampMin="0"))
+	double riseVelocity;
+	/** Fall velocity when not relaxed */
+	UPROPERTY(EditAnywhere, meta = (ClampMax="0"))
+	double fallVelocity;
+	/** Required rate of the values corresponding to the opposite state to change state (relaxed state > 50 not relaxed state < 50) */
+	UPROPERTY(EditDefaultsOnly, meta = (ClampMin="0", ClampMax="1", UIMin="0", UIMax="1"))
+	float oppositeStateThreshold;
+	/** Time/Duration it should take to reach the target velocity (rise or fall velocity) when changing state */
+	UPROPERTY(EditAnywhere, meta = (ClampMin="0"))
+	float interpDuration;
+	/** Current relaxation value, based on the current frame's interpolation between m_prevAvg and m_currAvg */
+	UPROPERTY(BlueprintReadOnly)
+	float relaxationValue;
+	/** Number of stored relaxation value, decides how much values should be used to compute the relaxation average */
+	UPROPERTY(BlueprintReadOnly, EditAnywhere, meta = (ClampMin="1"))
+	int meditationQueueSize;
+	UPROPERTY(BlueprintReadOnly)
+	bool bRelaxed;
+
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
+
+	/**
+	 * Calculates the new up velocity and sets the characters z-velocity with it.
+	 * @param DeltaSeconds	DeltaTime
+	 */
+	void UpdateUpVelocity(float DeltaSeconds);
+	/**
+	 * Called when Landing to launch the player again if he is in relaxed and rising state. 
+	 */
+	UFUNCTION()
+	void Landed(const FHitResult& Hit);
 
 public:	
 	// Called every frame
@@ -35,6 +77,28 @@ public:
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
+	/**
+	 * Evaluates whether or not bRelaxed should change.
+	 * @return True if bRelaxed should get inverted. False otherwise.
+	 */
 	UFUNCTION(BlueprintCallable)
-	bool ShouldStopRelaxed(float DeltaTime);
+	bool ShouldChangeState();
+	/**
+	 * Registers a new value into m_meditationValues, and pops/deletes the last one.
+	 * @param value			New value to be registered
+	 */
+	UFUNCTION(BlueprintCallable)
+	void RegisterValue(float value);
+	/**
+	 * To use if the m_meditationValues array has only 1 or 2 values.
+	 * Assigns the one or two only values present in m_meditationValues to m_prevAvg and m_currAvg.
+	 * Basically pointless, this function exists just in case we want a flat value (?).
+	 */
+	UFUNCTION(BlueprintCallable)
+	void AssignValue();
+	/**
+	* Updates m_prevAvg and m_currAvg based on the just retrieved new value, and the past values together.
+	*/
+	UFUNCTION(BlueprintCallable)
+	void ComputeAvg();
 };
